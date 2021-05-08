a6fabbd692e59258bf0064c907dc8348218e38df
ngoc duc <ngocduc@ngocs-MacBook-Pro.local>
2021-05-08 21:34:18 +0700

    init

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ function sendMessage(senderId, message) {
   });
 }
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 5111);
 app.set('ip', process.env.IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
