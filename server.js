2f1ac76efa786dfbcac1c92c8b450cb7a8f79665
ducdn <ducdn@gmail.com>
2021-05-10 15:18:27 +0700

    update

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.post('/webhook', function (req, res) { // Phần sử lý tin nhắn của n
 
 // Đây là function dùng api của facebook để gửi tin nhắn
 function sendMessage(senderId, message) {
+    console.log('senderId, message', senderId, message)
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {
